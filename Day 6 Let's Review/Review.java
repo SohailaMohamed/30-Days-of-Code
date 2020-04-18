aac1ab8b396b8805072f68e116bd838be974c742
Sohaila Mohamed <soomohamed123@outlook.com>
2020-04-18 23:32:52 +0200

    Update Review.java

--- Day 6 Let's Review/Review.java
+++ Day 6 Let's Review/Review.java
@@ -1,1 +1,28 @@
-sdffdslkjhslkjhljks
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int j = scan.nextInt();
+        for(int ii=0; ii<j; ii++){
+        String s = scan.next();
+        String odd="", even="";
+        char []c = s.toCharArray();
+        for(int i=0; i<c.length; i++) {
+            if(i%2==0) {
+                even=even+c[i];
+            }else {
+                odd=odd+c[i];
+            }
+        }
+        System.out.println(even+" "+odd);
+        }
+        scan.close();
+    }
+}
